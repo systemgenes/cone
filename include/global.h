ab92eadbf8b22d80af82ff94ea2987dec580f07a
Ravi Kant Pandey <ravi.p@greyorange.sg>
2018-11-07 15:04:41 +0530

    Added initial project structure.

---
+++ include/global.h
@@ -0,0 +1,6 @@
+#ifndef __GLOBAL_H__
+#define __GLOBAL_H__
+
+const char g_version[] = "0.0";
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+#include "global.h"
+
+int main() {
+	printf("Cone v%s Setup Starting!\n", g_version);
+	printf("Finished.\n");
+	return 0;
+}
